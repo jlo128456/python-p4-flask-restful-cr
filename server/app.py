2116c2259cfc7daa9814f97ef485006f27d896e2
Jeffrey <j.lo128456@gmail.com>
2025-04-21 19:02:08 +1000

    completed lab

--- server/app.py
+++ server/app.py
@@ -17,7 +17,44 @@ db.init_app(app)
 api = Api(app)
 
 class Home(Resource):
-    pass
+    def get(self):
+        return make_response(
+            {"message": "Welcome to the Newsletter RESTful API"},
+            200
+        )
+
+api.add_resource(Home, '/')
+
+class Newsletters(Resource):
+    def get(self):
+        newsletters = [n.to_dict() for n in Newsletter.query.all()]
+        return make_response(newsletters, 200)
+
+    def post(self):
+        data = request.get_json()  # Use JSON data
+
+        new_record = Newsletter(
+            title=data.get('title'),
+            body=data.get('body')
+        )
+
+        db.session.add(new_record)
+        db.session.commit()
+
+        return make_response(new_record.to_dict(), 201)
+
+api.add_resource(Newsletters, '/newsletters')
+
+class NewsletterByID(Resource):
+    def get(self, id):
+        newsletter = Newsletter.query.get(id)
+
+        if not newsletter:
+            return make_response({"error": "Newsletter not found"}, 404)
+
+        return make_response(newsletter.to_dict(), 200)
+
+api.add_resource(NewsletterByID, '/newsletters/<int:id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
